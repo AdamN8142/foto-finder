66cea57be4dfe23da09d8884a04a54ef27e29cbf
Adam Niedzwiecki <adamn8142@gmail.com>
2018-11-03 19:00:58 -0600

    Saves in array

--- main.js
+++ main.js
@@ -8,30 +8,63 @@ var cardContainer    = document.querySelector('.card-container')
 
 
   addToAlbumButton.addEventListener('click', createNewCard);
+  cardContainer.addEventListener('click', removeCard);
 
+var photosArray = [];
 
 function createNewCard(){
   var title = titleInput.value;
   var caption = captionInput.value;
-  var photo = new Photo(0,0,title,caption,0);
-  var cardContent = document.createElement('article')
+  var file = chooseFileBtn.files[0];
+  var photoUrl = URL.createObjectURL(file);
+  var photo = new Photo(0,photoUrl,title,caption,0);
+  var cardContent = document.createElement('article');
+  cardContent.dataset.id =`${photo.id}`
+  cardContent.classList.add('card')
   cardContent.innerHTML =  
-
-         `<h3 class = 'card-title'>${photo.title}</h3>
-          <div class = 'card-pic image'></div>
-          <p class = 'card-text'>${photo.caption}</p>
-          <section class = 'bottom-of-card'>
-            <div class = 'icon-section'>
-              <img class = 'delete-icon icon'src="Images/delete.svg">
-              <img class = 'favorite-icon icon'src="Images/favorite.svg">
-            </div> 
-          </section>`;
-       
-        addCardToPage(cardContent);
+        `
+           <h3 class = 'card-title'>${photo.title}</h3>
+            <img src = '${photo.file}' class = 'image'>
+            <p class = 'card-text'>${photo.caption}</p>
+            <section class = 'bottom-of-card'>
+              <div class = 'icon-section'>
+                <button class = 'delete-icon icon'></button>
+                <button class = 'favorite-icon icon'></button>
+              </div> 
+            </section>
+         `;
+  addCardToPage(cardContent);
+  // addToLocalStorage(photo)
+  photo.saveToStorage(photosArray,true)
 }
 
 
-  function addCardToPage(cardContent){
-    cardContainer.prepend(cardContent);
+function addCardToPage(cardContent){
+  cardContainer.prepend(cardContent);
+
+}
+
+
+
+  function removeCard(){
+  if (event.target.classList.contains('delete-icon')) {
+    var cardElement = event.target.closest('.card');
+    var photoID = parseInt(cardElement.dataset.id);
+    var index = photosArray.findIndex(function(photo){
+      return photo.id = photoID
+    })
+    photosArray[index].deleteFromStorage(photosArray,index)
+    cardElement.remove();
+    
+    } 
+
   }
+
+  // function addToLocalStorage(photo){
+//   var stringifiedObject = JSON.stringify(photo)
+//   photo.saveToStorage(stringifiedObject);
+// }
+
+
+
 
--- photo.js
+++ photo.js
@@ -1,7 +1,7 @@
 class Photo {
   
   constructor(id,file,title,caption,favorite){
-    this.id =id;
+    this.id = Date.now()|| id; 
     this.file = file;
     this.title = title;
     this.caption = caption;
@@ -9,17 +9,40 @@ class Photo {
 
   }
 
-
-saveToStorage(){
-
-}
-
-deleteFromStorage(){
-
-}
-
-updatePhoto(){
-    
+ 
+  updatePhoto(){
+      
   }
 
+  saveToStorage(photosArray, isNewCard){
+    if(isNewCard===true){
+      photosArray.push(this)
+    } localStorage.setItem('allPhotos',JSON.stringify(photosArray))
+  }
+
+
+  deleteFromStorage(photosArray, index){
+    photosArray.splice(index,1)
+    this.saveToStorage(photosArray)
+  }
 }
+
+
+  // static deleteFromStorage(id){
+  //   var photos = localStorage.getItem('photosObject');
+  //   photos = JSON.parse(photos);
+  //   delete photos[id];
+  //   photos = JSON.stringify(photos);
+  //   localStorage.setItem('photosObject', photos); 
+  // }
+
+   // saveToStorage(stringifiedObject){
+  //   var photos = localStorage.getItem('photosObject') || '{}'
+  //   // We do this because if theres nothing in local storage, it will give us back null 
+  //   photos = JSON.parse(photos);
+
+  //   photos[this.id] = stringifiedObject;
+
+  //   photos = JSON.stringify(photos);
+  //   localStorage.setItem('photosObject', photos); 
+  // }
